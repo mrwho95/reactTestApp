e24a01032262c6c627f4bd69bdfb98b810fccd8b
mrwho95 <dnkh903@gmail.com>
2020-05-18 02:32:37 +0800

    build signIn and Sign up components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from "./components/project/ProjectDetails";
 import ProjectSummary from "./components/project/ProjectSummary";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 
 function App() {
   return (
@@ -29,6 +31,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Dashboard} />
           <Route path="/project/:id" component={ProjectDetails} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>
